e4ea2aa80fbbd4aff6c89a14e179a7c67fd5c129
Ryan Greyling <greyl001@umn.edu>
2021-07-07 19:39:16 -0500

    Move History and Game Setup

--- src/App.tsx
+++ src/App.tsx
@@ -101,11 +101,52 @@ function Bush(props: BushProps) {
       {isClicked 
       ? <Button className="rose-button" variant="contained" color="secondary" onClick={() => gameUpdate(1)}>Put Back</Button>
       : props.colorsCapped || props.count === 0
-        ? <Button className="rose-button"  variant="outlined" disabled>Colors Capped</Button>
+        ? <Button className="rose-button"  variant="outlined" disabled>{props.count === 0 ? "Empty" : "Colors Capped"}</Button>
         : <Button className="rose-button"  variant="contained" color="primary" onClick={() => gameUpdate(-1)}>Pick Rose</Button>
       }
     </div>
   );
+}
+
+type RoseCounterProps = {
+  count: number,
+  color: string,
+  setCount: (count: number) => void,
+};
+
+function RoseCounter(props: RoseCounterProps) { // Counter used to help setup new games
+  const setCount = props.setCount;
+
+  return (
+    <>
+    <div>{props.color}</div>
+    {props.count}
+    <button onClick={()=> {if (props.count !== 0) setCount(props.count-1)}}>{"-"}</button>
+    <button onClick={()=> {setCount(props.count+1)}}>{"+"}</button>
+    </>
+  )
+}
+
+function GameSetup(props: {init: colorSet, setGameStateSetup: (init: colorSet) => void}) {
+  const init = props.init;
+  let initKeyArr = Object.keys(init) as Array<keyof colorSet>;
+
+  const [game_setup_cache, setGameSetupCache] = useState(init); // Cache updates to the initial game state
+
+  return (
+    <>
+    {initKeyArr.map((key) => <RoseCounter key={key + "Setup"} count={game_setup_cache[key]} color={key} 
+      setCount={(count) => {
+        let new_game_setup_cache = {} as colorSet;
+        Object.assign(new_game_setup_cache, game_setup_cache);
+        new_game_setup_cache[key] = count;
+        setGameSetupCache(new_game_setup_cache);
+      }}/>)}
+      <Button onClick={() => props.setGameStateSetup(game_setup_cache)}>
+        Setup New Game
+      </Button>
+    </>
+  )
 }
 
 type colorSet = {
@@ -125,9 +166,10 @@ type colorSetBool = {
   orange: boolean,
   cyan: boolean,
 };
+
 // Main component
 function Game() {
-let game_state_init: colorSet = {// Game State: Roses remaining on each Bush
+let game_state_default: colorSet = {// Game State: Roses remaining on each Bush
   red: 5,
   blue: 5,
   green: 5,
@@ -153,16 +195,35 @@ let button_is_clicked_init: colorSetBool = {// Controls the state of the button 
   cyan: false,
 };
 
+const [game_state_init, setGameStateSetup] = useState(game_state_default);
 const [game_state, setGameState] = useState(game_state_init);
 const [game_buffer, setGameBuffer] = useState(buffer_init); // buffer to cache potential moves
+const [game_history, setGameHistory] = useState([game_state_init]); // history containing move history
+const [game_history_index, setGameHistoryIndex] = useState(0); // current index into game_history
+
 const [button_is_clicked, setButtonIsClicked] = useState(button_is_clicked_init);
 const [curr_player, setCurrPlayer] = useState(true);
 const [colorsCapped, setColorsCapped] = useState(false); // whether or not a player has already picked 2 colors for their move
 const [gameOver, setGameOver] = useState(false); // whether or not the game is over
 
-useEffect(() => {
-    "pass";
-}, [game_state]);
+useEffect(() => { // handles changes to game_state_init
+  setGameHistory([game_state_init]);
+  setGameHistoryIndex(0);
+}, [game_state_init])
+
+useEffect(() => { // handle changes to game_history_index
+  // update game_state
+  setGameState(game_history[game_history_index]);
+
+  // cleanup
+  setGameBuffer(buffer_init);
+  setCurrPlayer(game_history_index % 2 === 0);
+  setColorsCapped(false);
+  setButtonIsClicked(button_is_clicked_init);
+
+  let remaining_roses = Object.values(game_state).reduce((acc,val) => acc+=val, 0);
+  if (remaining_roses === 0) setGameOver(true);
+}, [game_history_index, game_history]);
 
 let stateKeyArr = Object.keys(game_state) as Array<keyof colorSet>;
 let bufferKeyArr = Object.keys(game_buffer) as Array<keyof colorSet>;
@@ -175,13 +236,14 @@ return (
     <div className="flexbox-bushes">{stateKeyArr.map((key) => 
       <Bush key={key} color={key} count={game_state[key]} colorsCapped={colorsCapped} isClicked={button_is_clicked[key]}
         gameUpdate={(change) => { 
-              // Add potential moves to game_state and game_buffer
+              // New game state
               let new_game_state = {} as colorSet;
-              let new_game_buffer = {} as colorSet;
               Object.assign(new_game_state, game_state);
               new_game_state[key] += change;
               setGameState(new_game_state);
 
+              // New game buffer
+              let new_game_buffer = {} as colorSet;
               Object.assign(new_game_buffer, game_buffer);
               new_game_buffer[key] -= change;
               setGameBuffer(new_game_buffer);
@@ -213,14 +275,11 @@ return (
     <br/>
 
     <div className="update">
-      <Button className="update-button" variant="outlined" onClick={() => {// Move buffered changes into game state
-                setGameBuffer(buffer_init);
-                setCurrPlayer(!curr_player);
-                setColorsCapped(false);
-                setButtonIsClicked(button_is_clicked_init);
-
-                let remaining_roses = Object.values(game_state).reduce((acc,val) => acc+=val, 0);
-                if (remaining_roses === 0) setGameOver(true);
+      <Button className="update-button" variant="outlined" onClick={() => {
+              // Add current game_state to history and increment index
+              let new_index = game_history_index + 1;
+              setGameHistory([...game_history.slice(0, new_index), game_state]);
+              setGameHistoryIndex(new_index);
         }}>
         Update State
       </Button>
@@ -237,7 +296,21 @@ return (
       </ol>
       The Winner is the player who takes the final rose. 
     </div>
-
+    {game_history_index !== 0 ?
+      <Button onClick={() => setGameHistoryIndex(game_history_index - 1)}>
+        Previous Move
+      </Button>
+    :  <Button disabled>
+        Previous Move
+      </Button>}
+    {game_history_index !== game_history.length-1 ?
+      <Button onClick={() => setGameHistoryIndex(game_history_index + 1)}>
+        Next Move
+      </Button>
+    : <Button disabled>
+        Next Move
+      </Button>}
+      <GameSetup init={game_state_init} setGameStateSetup={setGameStateSetup}/>
   </>
 )
 
